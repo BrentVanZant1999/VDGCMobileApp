38d974940a4865459d2f01ff628cb153b788dee8
BrentVanzant1999 <bvancar@yahoo.com>
2019-01-24 18:03:09 -0800

    Small Comit
    Added in a scene illustrating basic single axis based movement.  Feel free to play around and learn about materials, 3d objects, and the character controller.

---
+++ VDGCIntroToUnity/Assets/Scripts/PlayerContr.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContr : MonoBehaviour
+{
+    //some variables for the player.
+    public int playerHorizontlalSpeed = 10;
+    public int playerJumpSpeed = 200;
+
+    //controller reference 
+    public CharacterController myController;
+
+    //movement holder 
+    public Vector3 movementVector; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //simple velocity set. 
+        movementVector = new Vector3(Input.GetAxis("Horizontal") * playerHorizontlalSpeed, 0f, 0);
+
+        bool jumpDown = Input.GetButtonDown("Jump");
+
+        //handle jumping logic
+        if (jumpDown)
+        {
+
+        }
+
+        //handle side logic 
+
+
+        //handle basic gravity 
+        movementVector.y = movementVector.y + Physics.gravity.y;
+        //utilize deltaTime 
+        myController.Move(movementVector*Time.deltaTime);
+    }
+}
