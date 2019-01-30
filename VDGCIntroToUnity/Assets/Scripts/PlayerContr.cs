b5eeaf2bbe0434457f0dc4c4312afcab9cf6eb24
BrentVanZant <bvanzant@ucsd.edu>
2019-01-29 17:16:17 -0800

    BoundarySystemExample
    Added in a simple boundary checking

--- VDGCIntroToUnity/Assets/Scripts/PlayerContr.cs
+++ VDGCIntroToUnity/Assets/Scripts/PlayerContr.cs
@@ -5,41 +5,56 @@ using UnityEngine;
 public class PlayerContr : MonoBehaviour
 {
     //some variables for the player.
-    public int playerHorizontlalSpeed = 10;
-    public int playerJumpSpeed = 200;
+    private float playerHorizontlalSpeed = 10;
+    private float playerJumpSpeed = 200;
+    private float myPosX;
+    private float myPosY;
+    private float myPosZ;
+
+    public float rightBoundary;
+    public float leftBoundary; 
 
     //controller reference 
     public CharacterController myController;
 
     //movement holder 
-    public Vector3 movementVector; 
+    private Vector3 movementVector; 
 
     // Start is called before the first frame update
     void Start()
     {
         myController = GetComponent<CharacterController>();
+        myPosX = myController.transform.position.x;
+        myPosY = myController.transform.position.y;
+        myPosZ = myController.transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //simple velocity set. 
-        movementVector = new Vector3(Input.GetAxis("Horizontal") * playerHorizontlalSpeed, 0f, 0);
-
+        myPosX = myController.transform.position.x;
+        myPosY = myController.transform.position.y;
+        myPosZ = myController.transform.position.z;
+        //check to see if the player is inputing horizontal movement
+        float isMovingHorizontally = Input.GetAxis("Horizontal");
+        //check to see if the player is current holding the jump button down. 
         bool jumpDown = Input.GetButtonDown("Jump");
 
-        //handle jumping logic
-        if (jumpDown)
-        {
 
-        }
-
-        //handle side logic 
-
+        movementVector = new Vector3(isMovingHorizontally * playerHorizontlalSpeed, 0f, 0);
 
         //handle basic gravity 
         movementVector.y = movementVector.y + Physics.gravity.y;
         //utilize deltaTime 
-        myController.Move(movementVector*Time.deltaTime);
+        myController.Move(movementVector*Time.deltaTime); 
+        outofbounds();
+
+    }
+    //function to handle bounds.
+    void outofbounds()
+    {
+        if (transform.position.x < leftBoundary) { transform.position = new Vector3(leftBoundary, myPosY, myPosZ); }
+        if (transform.position.x > rightBoundary) { transform.position = new Vector3(rightBoundary, myPosY, myPosZ); }
+
     }
 }
